f06d76cd4ee7d6e9597fc875592bf824912adb98
yusef02 <youssef.bouziane2402@gmail.com>
2024-02-26 21:58:20 +0100

    stampa schermo dei valori per ogni hotel

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once './main.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +10,16 @@
   <title>hotel db</title>
 </head>
 <body>
-  
+  <ul>
+    <?php foreach($hotels as $hotel): ?>
+      <li>
+        <?= $hotel['name'] ?>
+        <?= $hotel['description'] ?>
+        <?= $hotel['parking'] ?>
+        <?= $hotel['vote'] ?>
+        <?= $hotel['distance_to_center'] ?>
+      </li>
+      <?php endforeach; ?>
+  </ul>
 </body>
 </html>
